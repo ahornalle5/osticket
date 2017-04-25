6f559937d89946ed05a473af680a7110a441f490
Cieslicki Michal <CieslickiM@vaw-ggmbh.de>
2017-04-25 14:16:24 +0200

    Neue Funktion log2file() zum Debuggen mit der Hilfe von Dateien, die im "uploads" Verzeichnis gespeichert werden können

--- include/class.config.php
+++ include/class.config.php
@@ -146,6 +146,12 @@ class Config {
 
         return $this->items;
     }
+    function log2file($logText, $fileName = null) {
+        date_default_timezone_set('Europe/Berlin');
+        $file = '/var/www/osticket/uploads/test'.$fileName.'.txt';
+        $body = date('Y-m-d H:i:s')." $logText\n";
+        file_put_contents($file, $body, FILE_APPEND);
+    }
 }
 
 class ConfigItem
