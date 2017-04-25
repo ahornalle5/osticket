50b2288e6fa82798c510318dd41ab753ded62053
Cieslicki Michal <CieslickiM@vaw-ggmbh.de>
2017-04-25 16:20:11 +0200

    Spracheinstellungen

---
+++ include/i18n/de_DE/MANIFEST.php
@@ -0,0 +1,8 @@
+<?php return array (
+  'Build-Date' => 'Mon, 30 Jan 17 20:25:31 +0000',
+  'Build-Version' => 'v1.10.00-0-901e5ea',
+  'Language' => 'de_DE',
+  'Id' => 'lang:de',
+  'Last-Revision' => '2017-01-30 14:46-0400',
+  'Version' => 145919,
+);
